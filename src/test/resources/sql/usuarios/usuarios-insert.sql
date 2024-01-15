c67fd69b2635ab90301e9044d2a573b60fd7113e
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-01-15 13:53:46 -0300

    Test ambients - part 1

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+insert into USUARIOS(id, username, password, role) values (100, 'ana@gmailcom','123456', 'ROLE_ADMIN');
+insert into USUARIOS(id, username, password, role) values (101, 'bia@gmailcom','123456', 'ROLE_CLIENTE');
+insert into USUARIOS(id, username, password, role) values (102, 'bob@gmailcom','123456', 'ROLE_CLIENTE');
