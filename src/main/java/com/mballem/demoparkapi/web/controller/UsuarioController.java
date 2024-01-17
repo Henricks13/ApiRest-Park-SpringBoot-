4561ee46e852bf5341b397e2b4ebcab2e27f69fe
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-01-17 09:11:08 -0300

    Access admin configuration with getByID

--- src/main/java/com/mballem/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/demoparkapi/web/controller/UsuarioController.java
@@ -17,6 +17,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -51,6 +52,7 @@ public class UsuarioController {
                     @ApiResponse(responseCode = "404", description = "Recurso não encontrado",
                             content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
             })
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
