15d739b26f0cab89bc0e5954c2d744a669b1508c
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-01-18 10:13:17 -0300

    Cliente Service,Repository,Dto and Cpf exception

---
+++ src/main/java/com/mballem/demoparkapi/service/ClienteService.java
@@ -0,0 +1,30 @@
+package com.mballem.demoparkapi.service;
+
+import com.mballem.demoparkapi.entity.Cliente;
+import com.mballem.demoparkapi.exception.CpfUniqueViolationException;
+import com.mballem.demoparkapi.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    @Transactional
+    public Cliente salvar (Cliente cliente){
+        try {
+
+            return clienteRepository.save(cliente);
+
+        }
+        catch (DataIntegrityViolationException ex){
+            throw new CpfUniqueViolationException(
+                    String.format("CPF '%s' não pode ser cadastrado, já existe no sistema",cliente.getCpf()));
+        }
+    }
+
+}
