00dbfe17e0ad84695e1038baa400cb6b8aff0853
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-01-20 10:08:59 -0300

    VagaService

---
+++ src/main/java/com/mballem/demoparkapi/service/VagaService.java
@@ -0,0 +1,36 @@
+package com.mballem.demoparkapi.service;
+
+import com.mballem.demoparkapi.entity.Vaga;
+import com.mballem.demoparkapi.exception.CodigoUniqueViolationException;
+import com.mballem.demoparkapi.exception.EntityNotFoundException;
+import com.mballem.demoparkapi.repository.VagaRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service @RequiredArgsConstructor
+public class VagaService {
+
+    private final VagaRepository vagaRepository;
+@Transactional
+    public Vaga salvar (Vaga vaga){
+        try {
+            return vagaRepository.save(vaga);
+        }
+        catch (DataIntegrityViolationException ex){
+            throw new CodigoUniqueViolationException(
+                    String.format("Vaga com codigo '%s' já cadastrada", vaga.getCodigo())
+            );
+
+        }
+    }
+@Transactional
+    public Vaga buscarPorCodigo(String codigo){
+    return vagaRepository.findByCodigo(codigo).orElseThrow(
+            () -> new EntityNotFoundException(String.format("Vaga com código '%s' não foi encontrada", codigo))
+    );
+    }
+
+}
