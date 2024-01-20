e41976337aa54eda2532234a3f5d6035529d5a59
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-01-20 09:58:53 -0300

    Dto's and Mapper with Vaga

--- src/main/java/com/mballem/demoparkapi/repository/VagaRepository.java
+++ src/main/java/com/mballem/demoparkapi/repository/VagaRepository.java
@@ -4,5 +4,5 @@ import com.mballem.demoparkapi.entity.Vaga;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VagaRepository extends JpaRepository<Vaga, Long> {
-    
+
 }
