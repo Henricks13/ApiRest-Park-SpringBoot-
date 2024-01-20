8b5ca309c7694483072677725b1c0ee395c45811
Ryan Henrick Da Silva Pinto <henrick.s1320@gmail.com>
2024-01-20 09:52:18 -0300

    Entity Vaga

---
+++ src/main/java/com/mballem/demoparkapi/repository/VagaRepository.java
@@ -0,0 +1,8 @@
+package com.mballem.demoparkapi.repository;
+
+import com.mballem.demoparkapi.entity.Vaga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VagaRepository extends JpaRepository<Vaga, Long> {
+    
+}
